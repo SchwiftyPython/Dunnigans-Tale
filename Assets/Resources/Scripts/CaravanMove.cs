973108bc0b145b435e8dd66db9c967867598991b
Brian <bhannasauro@hotmail.com>
2017-02-12 12:21:44 -0500

    Implemented prototype graphics and parallax scrolling

---
+++ Assets/Resources/Scripts/CaravanMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaravanMove : MonoBehaviour {
+	float curPos;
+
+	void Start () {
+		curPos = transform.position.x;
+		QualitySettings.vSyncCount = 0;
+	}
+	
+
+	void Update () {
+		if (Time.timeScale != 0) {
+			transform.Translate (-CaravanController.instance.wagonSpeed * Time.deltaTime, 0, 0);
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour {
+
+	private Transform cameraTransform;
+	private Transform[] layers;
+	private float viewZone = 5;
+	private int leftIndex;
+	private int rightIndex;
+	private float lastCameraX;
+
+	public float backgroundSize;
+	public float paralaxSpeed;
+
+	private void Start(){		
+		cameraTransform = Camera.main.transform;
+		lastCameraX = cameraTransform.position.x;
+		layers = new Transform[transform.childCount];
+
+		for (int i = 0; i < transform.childCount; i++) {
+			layers [i] = transform.GetChild (i);
+		}
+
+		leftIndex = 0;
+		rightIndex = layers.Length - 1;
+	}
+
+	private void Update(){
+		float deltaX = cameraTransform.position.x - lastCameraX;
+		transform.position += Vector3.right * (deltaX * paralaxSpeed);
+		lastCameraX = cameraTransform.position.x;
+
+		if (cameraTransform.position.x < (layers [leftIndex].transform.position.x + viewZone)) {
+			scrollLeft ();
+		}
+	}
+
+	private void scrollLeft(){
+		int lastRight = rightIndex;
+		layers [rightIndex].position = Vector3.right * (layers [leftIndex].position.x - backgroundSize);
+		leftIndex = rightIndex;
+		rightIndex--;
+		if (rightIndex < 0) {
+			rightIndex = layers.Length - 1;
+		}
+	}
+}
